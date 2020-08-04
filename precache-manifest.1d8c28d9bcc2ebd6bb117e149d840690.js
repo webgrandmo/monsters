de9baa0f9a9c04a5cca22343bcb74458ad2ec0d0
Andriy Trofimov <andriy.trofimov@carus.com>
2020-08-04 19:57:39 +0300

    Updates

---
+++ precache-manifest.1d8c28d9bcc2ebd6bb117e149d840690.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd48b768b0fba42e92db3526461a1ca5",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "a34b54caa973f880ff51",
+    "url": "/monsters/static/css/main.cd367d5c.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a34b54caa973f880ff51",
+    "url": "/monsters/static/js/main.139479a7.chunk.js"
+  },
+  {
+    "revision": "3dab8aa598718cb8e8b3",
+    "url": "/monsters/static/js/runtime-main.f48c3991.js"
+  }
+]);
